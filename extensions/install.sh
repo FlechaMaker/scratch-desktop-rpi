ec083189e641223d9e0aba1feeec7ee75cc8c4f8
Yuchi Yahagi <yahagi@nae-lab.org>
2024-05-28 17:07:09 +0900

    Install speech2scratch (OpenAI API version)

--- extensions/install.sh
+++ extensions/install.sh
@@ -5,6 +5,6 @@ sh extensions/install_geoscratch.sh
 sh extensions/install_facemesh2scratch.sh
 sh extensions/install_posenet2scratch.sh
 sh extensions/install_tm2scratch.sh
-# sh extensions/install_speech2scratch.sh
+sh extensions/install_speech2scratch.sh
 sh extensions/install_scratch3-qrcode.sh
 sh extensions/install_chatgpt2scratch.sh
--- extensions/install_speech2scratch.sh
+++ extensions/install_speech2scratch.sh
@@ -1,16 +1,3 @@
 #! /bin/bash
 
-# Check if the patch is applicable
-git apply --check extensions/speech2scratch_main.patch
-if [ $? -eq 0 ]; then
-    echo "Applying patch extensions/speech2scratch_main.patch"
-    git apply extensions/speech2scratch_main.patch
-fi
-
-git apply --check extensions/speech2scratch.patch
-if [ $? -eq 0 ]; then
-    echo "Applying patch extensions/speech2scratch.patch"
-    git apply extensions/speech2scratch.patch
-fi
-
 sh node_modules/speech2scratch/install.sh
